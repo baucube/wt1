164582183681003d154797e27e8201b5eac476b5
baucube <barryboucar0204@gmail.com>
2022-08-01 10:07:40 +0000

    Comments

--- src/main/java/sn/baucube/Test.java
+++ src/main/java/sn/baucube/Test.java
@@ -1,6 +1,11 @@
+/**
+ * Comment
+ */
+
 package sn.baucube;
 
 public class Test{
+    //-----------------------------------
     public static void main(String[] args){
         System.out.println("Hello !");
     }
